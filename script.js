027ca0f5678399407d338e5d82be2970a17ef76d
Mauro César <mauroguima080@hotmail.com>
2024-11-06 13:17:20 -0300

    Criando menu hamburguer

--- script.js
+++ script.js
@@ -92,6 +92,11 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+/* Menu Hamburger */
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 
 
